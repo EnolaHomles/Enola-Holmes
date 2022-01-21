67e82a25ddfcd0457b22a1246ea563de8bbb8e38
Enola Holmes <97936974+EnolaHomles@users.noreply.github.com>
2022-01-21 08:56:21 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/EnolaHomles/Enola-Holmes
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /Enola-Holmes
 fi
-cd /EvaMaria
+cd /Enola-Holmes
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
